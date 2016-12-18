d81568865243d99ca48d15de4188bbb8ebfe409f
root <root@vps-1296162-x.dattaweb.com>
2016-12-17 21:23:17 -0300

    First commit

---
+++ app/Http/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|-------------------------------------------------------------------------
+|Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+// Ejemplos de ruta
+
+Route::get('nombre', function(){
+
+    return "Hola mundo";
+
+});
